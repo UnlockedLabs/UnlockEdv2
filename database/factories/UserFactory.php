b5fb6cf52b11419aa5c86f60e68ffa5ecf6fed86
PThorpe92 <preston@unlockedlabs.org>
2023-11-30 12:09:36 -0500

    feat: add role to user model and add migration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Enums\UserRole;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -26,7 +27,17 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => UserRole::Student,
         ];
+    }
+    /**
+     * User with Admin role
+     */
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => UserRole::Admin,
+        ]);
     }
 
     /**
