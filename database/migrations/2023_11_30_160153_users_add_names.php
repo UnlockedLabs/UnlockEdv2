f059f88aa8b7336dddef885e35373dc9e2bf9b8c
PThorpe92 <preston@unlockedlabs.org>
2023-12-01 13:13:40 -0500

    fix: move name_first + name_last to proper down() migration fn

--- database/migrations/2023_11_30_160153_users_add_names.php
+++ database/migrations/2023_11_30_160153_users_add_names.php
@@ -22,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name_first');
+            $table->dropColumn('name_last');
+        });
     }
 };
