3ed67abe16b224bebcae4fd6d2c9bd161c74f4ea
PThorpe92 <preston@unlockedlabs.org>
2023-12-01 15:46:27 -0500

    fix: make username field nullable for the CI

--- database/migrations/2023_11_30_224221_users_add_username.php
+++ database/migrations/2023_11_30_224221_users_add_username.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username');
+            $table->string('username')->nullable();
         });
     }
 
