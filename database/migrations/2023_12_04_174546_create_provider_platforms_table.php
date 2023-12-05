1ac986fa81795284f6cd3d35687701d4336abc83
Chris Santillan <chris@unlockedlabs.org>
2023-12-05 16:54:19 -0600

    feat: add model/migration for Provider Platforms UN-182

---
+++ database/migrations/2023_12_04_174546_create_provider_platforms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('provider_platforms', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('icon_url');
+            $table->string('account_id');
+            $table->string('access_key');
+            $table->string('base_url');
+            $table->string('state')->default('enabled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('provider_platforms');
+    }
+};
