5bbe64fa064acc6bab007098e6dfb4b1d154e5bd
Chris Santillan <chris@unlockedlabs.org>
2023-12-06 11:54:41 -0600

    feat: add factory/seeder for Provider Platforms UN-197

---
+++ database/seeders/ProviderPlatformSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProviderPlatform;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProviderPlatformSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        ProviderPlatform::factory(10)->create(); // Create 10 Provider Platforms using the factory
+    }
+}
