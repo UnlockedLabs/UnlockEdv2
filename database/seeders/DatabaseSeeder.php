a880c954bb413ff328cc04a7264edd16c6b88bc1
PThorpe92 <preston@unlockedlabs.org>
2023-12-08 13:03:58 -0500

    feat: edit resources + continued work on UN-144

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,12 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
-
-        /* \App\Models\User::factory()->create([ */
-        /*     'name_first' => 'Test', */
-        /*     'name_last' => 'User', */
-        /*     'email' => 'test@example.com', */
-        /* ]); */
+        User::factory(10)->create();
     }
 }
