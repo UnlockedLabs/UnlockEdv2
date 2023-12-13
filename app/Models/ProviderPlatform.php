76ebb034e1530ef4e78c4647d55ae8009bfebdfd
Chris Santillan <chris@unlockedlabs.org>
2023-12-13 11:14:50 -0600

    feat: add hashing function for `access_key`

--- app/Models/ProviderPlatform.php
+++ app/Models/ProviderPlatform.php
@@ -6,6 +6,8 @@ use App\Enums\ProviderPlatformState;
 use App\Enums\ProviderPlatformType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class ProviderPlatform extends Model
 {
@@ -31,4 +33,12 @@ class ProviderPlatform extends Model
         'type' => ProviderPlatformType::class,
         'state' => ProviderPlatformState::class,
     ];
+
+    public function hashAccessKey()
+    {
+        $accessKey = $this->access_key;
+        $hashedAccessKey = Hash::make($accessKey);
+        $this->access_key = $hashedAccessKey;
+        $this->save();
+    }
 }
