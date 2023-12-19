ca2d88fbf15905558a989573c1b6b004a1fba6db
PThorpe92 <preston@unlockedlabs.org>
2023-12-19 14:43:07 -0500

    chore: remove sanctum and enable passport + clean dependency add basic configurations

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:passport')->get('/user', function (Request $request) {
     return $request->user();
 });
 
