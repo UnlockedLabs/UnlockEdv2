b8b453e60f6048b94e8c9ac582d8ad656f65149a
coledykstra <cole.dykstra@maine.edu>
2024-01-04 13:21:07 -0500

    feat: create enrollment model and migration

---
+++ app/Models/Enrollment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Enrollment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'provider_id',
+        'enrollment_state',
+        'links',
+        'provider_start_at',
+        'provider_end_at',
+    ];
+
+    protected $casts = [
+        'links' => 'json',
+        'provider_start_at' => 'datetime',
+        'provider_end_at' => 'datetime',
+    ];
+}
---
+++ database/migrations/2024_01_04_181542_create_enrollments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnrollmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('course_id');
+            $table->string('provider_id')->nullable();
+            $table->enum('enrollment_state', ['active', 'inactive', 'completed', 'deleted']);
+            $table->json('links');
+            $table->timestamp('provider_start_at')->nullable();
+            $table->timestamp('provider_end_at')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('enrollments', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            // $table->foreign('course_id')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrollments');
+    }
+}
