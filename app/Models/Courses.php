6fa9c4e19d44d0445b602db54d5bada7b4142020
corypride <cory.pride83@gmail.com>
2024-01-04 10:48:43 -0600

    feat: UN-211 Courses Model and Migration

---
+++ app/Models/Courses.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Courses extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'provider_resource_id',
+        'provider_course_name',
+        'provider_start_at',
+        'provider_end_at',
+    ];
+}
