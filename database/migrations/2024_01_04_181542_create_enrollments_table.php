582865a12dcac2f33cdf23e459643b1572022c9a
coledykstra <cole.dykstra@maine.edu>
2024-01-04 13:28:40 -0500

    fix: update foreignId in enrollments

--- database/migrations/2024_01_04_181542_create_enrollments_table.php
+++ database/migrations/2024_01_04_181542_create_enrollments_table.php
@@ -15,8 +15,8 @@ class CreateEnrollmentsTable extends Migration
     {
         Schema::create('enrollments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('course_id');
+            $table->foreignId('user_id');
+            $table->foreignId('course_id');
             $table->string('provider_id')->nullable();
             $table->enum('enrollment_state', ['active', 'inactive', 'completed', 'deleted']);
             $table->json('links');
@@ -25,10 +25,6 @@ class CreateEnrollmentsTable extends Migration
             $table->timestamps();
         });
 
-        Schema::table('enrollments', function (Blueprint $table) {
-            $table->foreign('user_id')->references('id')->on('users');
-            // $table->foreign('course_id')->references('id')->on('courses');
-        });
     }
 
     /**
