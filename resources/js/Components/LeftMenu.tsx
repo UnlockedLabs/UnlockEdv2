721a7b89c7a79410175fa0f6b6ee28d8c54c3a0e
calisio <calisio@wustl.edu>
2024-01-16 13:55:24 -0600

    feat: Pull category data from api

--- resources/js/Components/LeftMenu.tsx
+++ resources/js/Components/LeftMenu.tsx
@@ -1,48 +1,69 @@
+import axios from "axios";
 import Brand from "./Brand";
 import {
     HomeIcon,
     ArchiveBoxIcon,
     BookOpenIcon,
 } from "@heroicons/react/24/solid";
+import useSWR from "swr";
 
 interface CategoryParameters {
     categoryName: string;
     linksArray: Array<LinksArrayParameters>;
+    rank: number;
 }
-// had to add undefined to this, but not sure if that is not allowed
 interface LinksArrayParameters {
-    [linkName: string]: string | undefined;
+    [linkName: string]: string;
 }
 
-function Category({categoryName, linksArray}:CategoryParameters) {
-    const linksList = linksArray.map((linkPair: { [x: string]: string | undefined; }) => {
+function Category({ categoryName, linksArray, rank }: CategoryParameters) {
+    const linksList = linksArray.map((linkPair: { [x: string]: string }) => {
         const key = Object.keys(linkPair)[0];
         return (
-            <li>
+            <li key={key.concat(rank.toString())}>
                 <a href={linkPair[key]}>{key}</a>
             </li>
-        )
-    })
-    return(
+        );
+    });
+    return (
         <li>
             <details>
                 <summary>
                     <ArchiveBoxIcon className="w-4" />
                     {categoryName}
                 </summary>
-                <ul>
-                    {linksList}
-                </ul>
+                <ul>{linksList}</ul>
             </details>
         </li>
-    )
+    );
 }
- 
+
+function getCategoryItems() {
+    const { data, error, isLoading } = useSWR("/api/v1/categories", (url) =>
+        axios.get(url).then((res) => res.data),
+    );
+    if (error) return <div>failed to load</div>;
+    if (isLoading) return <div>loading...</div>;
+    return data.data.map(
+        (category: {
+            name: string;
+            rank: number;
+            links: LinksArrayParameters[];
+        }) => {
+            return (
+                <Category
+                    key={category.rank}
+                    categoryName={category.name}
+                    linksArray={category.links}
+                    rank={category.rank}
+                />
+            );
+        },
+    );
+}
+
 export default function LeftMenu() {
-    const categoryData = [{ name: "Category 1", links: [{ "Link 1 Name": "http://link1.com" }, { "Link 2 Name": "http://link2.com" }] }, { name: "Category 2", links: [{ "Link 1 Name": "http://link1.com" }, { "Link 2 Name": "http://link2.com" }] }];
-    const categoryItems = categoryData.map((category) =>{
-        return (<Category categoryName={category.name} linksArray={category.links}/>)
-    });
+    const categoryItems = getCategoryItems();
 
     return (
         <ul className="menu bg-base-100 w-72">
