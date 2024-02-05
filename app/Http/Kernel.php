31306fcc7b1dffcf767e926638f1199655818644
PThorpe92 <preston@unlockedlabs.org>
2024-02-05 17:24:44 -0500

    fix: add scope middleware that merge conflict removed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'password_reset' => \App\Http\Middleware\PasswordReset::class,
+        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
+        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
     ];
 }
