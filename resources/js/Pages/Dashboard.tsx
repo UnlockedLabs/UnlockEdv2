fbea33becaf005762fb3fa694f9f75a48977d6e6
calisio <calisio@wustl.edu>
2024-02-27 13:58:26 -0600

    feat: adds courses UI onto dashboard with hardcoded data

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,11 +1,13 @@
 import PageNav from "@/Components/PageNav";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PageProps } from "@/types";
+import CourseContent from "@/Components/CourseContent";
 
 export default function Dashboard({ auth }: PageProps) {
     return (
         <AuthenticatedLayout user={auth.user} title="Dashboard">
             <PageNav user={auth.user} path={["Dashboard"]} />
+            <CourseContent />
         </AuthenticatedLayout>
     );
 }
