5bee49922ddf0cbc9bcaec5b458798c9dc7b5bb2
calisio <calisio@wustl.edu>
2024-03-01 16:39:20 -0600

    feat: adds description to each course card

--- resources/js/Components/CourseContent.tsx
+++ resources/js/Components/CourseContent.tsx
@@ -14,21 +14,28 @@ export default function CourseContent({ user }: { user: User }) {
             error: courseInfoError,
             isLoading: courseInfoLoading,
         } = useSWR(`/api/v1/courses/${course.course_id}`);
-        if (!courseInfoError && !courseInfoLoading) console.log(courseInfo);
         const {
             data: providerInfo,
             error: providerInfoError,
             isLoading: providerInfoLoading,
         } = useSWR(`/api/v1/provider-platforms/${course.provider_platform_id}`);
-        if (!providerInfoError && !providerInfoLoading)
-            console.log(providerInfo);
+
+        // Function to truncate the description to the first 100 characters
+        const truncateDescription = (description: string) => {
+            if (description.length > 100) {
+                return description.slice(0, 100) + "...";
+            } else {
+                return description;
+            }
+        };
         return (
-            <div>
+            // temp solution for height, make sure they are all the same height without fixing it
+            <div className="h-[400px]">
                 {!courseInfoError &&
                 !courseInfoLoading &&
                 !providerInfoLoading &&
                 !providerInfoError ? (
-                    <div className="card card-compact bg-base-100 shadow-xl">
+                    <div className="card card-compact bg-base-100 shadow-xl h-full">
                         <figure>
                             <img src={providerInfo.data.icon_url} alt="" />
                         </figure>
@@ -36,7 +43,11 @@ export default function CourseContent({ user }: { user: User }) {
                             <h2 className="card-title">
                                 {courseInfo.data.provider_course_name}
                             </h2>
-                            <p>{courseInfo.data.description}</p>
+                            <p>
+                                {truncateDescription(
+                                    courseInfo.data.description,
+                                )}
+                            </p>
                         </div>
                     </div>
                 ) : (
@@ -47,7 +58,7 @@ export default function CourseContent({ user }: { user: User }) {
     }
 
     return (
-        <div className="p-4 grid grid-cols-4 gap-5">
+        <div className="p-4 grid grid-cols-3 gap-5 w-[85%]">
             {!isLoading && !error ? (
                 enrollments.data.map((course: any) => (
                     <CourseCard
