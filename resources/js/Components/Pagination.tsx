6433461664f839875cf6f8bd35af818881bc3da6
Nokie Rae <nokie.rae@gmail.com>
2024-03-04 20:06:40 -0600

    fix: Fix pagination

--- resources/js/Components/Pagination.tsx
+++ resources/js/Components/Pagination.tsx
@@ -24,35 +24,39 @@ export default function Pagination({
     meta: PaginationMeta;
     setPage: (page: number) => void;
 }) {
+    const page = meta.current_page - 1;
+
     return (
         <div className="join place-content-center">
             <div className="tooltip tooltip-left" data-tip="First Page">
                 <button
                     className="join-item btn btn-sm"
-                    onClick={() => setPage(meta.from)}
+                    onClick={() => setPage(1)}
                 >
                     <ChevronDoubleLeftIcon className="h-4" />
                 </button>
             </div>
 
-            {[...Array(meta.last_page).keys()].map((_, i) => {
-                return (
-                    <button
-                        className={`join-item btn btn-sm ${
-                            i == meta.current_page - 1 ? "btn-active" : ""
-                        }`}
-                        onClick={() => setPage(i + 1)}
-                        key={i}
-                    >
-                        {i + 1}
-                    </button>
-                );
-            })}
+            {[page - 2, page - 1, page, page + 1, page + 2]
+                .filter((i) => i >= 0 && i < meta.last_page)
+                .map((i) => {
+                    return (
+                        <button
+                            className={`join-item btn btn-sm ${
+                                i == meta.current_page - 1 ? "btn-active" : ""
+                            }`}
+                            onClick={() => setPage(i + 1)}
+                            key={i}
+                        >
+                            {i + 1}
+                        </button>
+                    );
+                })}
 
             <div className="tooltip tooltip-right" data-tip="Last Page">
                 <button
                     className="join-item btn btn-sm"
-                    onClick={() => setPage(meta.to)}
+                    onClick={() => setPage(meta.last_page)}
                 >
                     <ChevronDoubleRightIcon className="h-4" />
                 </button>
