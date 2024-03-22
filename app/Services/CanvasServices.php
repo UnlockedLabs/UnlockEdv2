06e1ba2d0d9148c289b3180b932b6a805127626e
PThorpe92 <preston@unlockedlabs.org>
2024-03-22 16:23:45 -0400

    fix: try to correct provider behavior w/ access_key on remote deployments

--- app/Services/CanvasServices.php
+++ app/Services/CanvasServices.php
@@ -84,7 +84,11 @@ class CanvasServices
 
         $this->provider_id = $providerId;
         $this->account_id = $accountId;
-        $this->access_key = Crypt::decryptString($apiKey);
+        try {
+            $this->access_key = Crypt::decryptString($apiKey);
+        } catch (\Illuminate\Contracts\Encryption\DecryptException $e) {
+            $this->access_key = $apiKey;
+        }
         $this->base_url = $url;
         $this->client = new Client();
     }
