ffaa57db60c7c07b432f68d980d0fed3021d279b
calisio <calisio@wustl.edu>
2024-03-29 16:43:12 -0500

    refactor: removes duplicate input folder

--- resources/js/Components/forms/inputs/SubmitButton.tsx
+++
@@ -1,8 +0,0 @@
-export function SubmitButton({ errorMessage }: { errorMessage: string }) {
-    return (
-        <label className="form-control pt-4">
-            <input className="btn btn-primary" type="submit" />
-            <div className="text-error text-center pt-2">{errorMessage}</div>
-        </label>
-    );
-}
