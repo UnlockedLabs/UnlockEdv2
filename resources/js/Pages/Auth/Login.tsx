b82bc00475a765e15f9ccaf346b4ffd5523521ba
Chris Conner <christopher.conner@maine.edu>
2024-04-01 21:43:38 -0400

    feat: refactor forms into their own components

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,40 +1,8 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-import Checkbox from "@/Components/inputs/Checkbox";
+import LoginForm from "@/Components/forms/LoginForm";
 import GuestLayout from "@/Layouts/GuestLayout";
-import InputError from "@/Components/InputError";
-import PrimaryButton from "@/Components/PrimaryButton";
-import { TextInput } from "@/Components/inputs/TextInput";
-import { Head, Link } from "@inertiajs/react";
-type Inputs = {
-    username: string;
-    password: string;
-    remember: boolean;
-};
+import { Head } from "@inertiajs/react";
 
 export default function Login({ status }: { status?: string }) {
-    const [errorMessage, setErrorMessage] = useState("");
-    const [processing, setProcessing] = useState(false);
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<Inputs>();
-
-    const submit: SubmitHandler<Inputs> = async (data) => {
-        try {
-            setErrorMessage("");
-            setProcessing(true);
-            await axios.post(route("login", data));
-            window.location.replace(route("dashboard"));
-        } catch (error: any) {
-            setProcessing(false);
-            setErrorMessage(error.response.data.message);
-        }
-    };
-
     return (
         <GuestLayout>
             <Head title="Log in" />
@@ -43,57 +11,7 @@ export default function Login({ status }: { status?: string }) {
                     {status}
                 </div>
             )}
-
-            <form onSubmit={handleSubmit(submit)}>
-                <div className="mt-4">
-                    <TextInput
-                        label={"Username"}
-                        interfaceRef={"username"}
-                        required={true}
-                        length={50}
-                        errors={errors}
-                        register={register}
-                    />
-                    <div className="h-6">
-                        <InputError message={errorMessage} className="pt-2" />
-                    </div>
-                </div>
-
-                <div className="mt-4">
-                    <TextInput
-                        label={"Password"}
-                        interfaceRef={"password"}
-                        required={true}
-                        length={50}
-                        errors={errors}
-                        register={register}
-                        password={true}
-                    />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            label={"Remember me"}
-                            interfaceRef={"remember"}
-                            register={register}
-                        />
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton
-                        className="ms-4 w-24 h-10"
-                        disabled={processing}
-                    >
-                        {processing ? (
-                            <span className="loading loading-spinner loading-sm mx-auto"></span>
-                        ) : (
-                            <div className="m-auto">Log in</div>
-                        )}
-                    </PrimaryButton>
-                </div>
-            </form>
+            <LoginForm />
         </GuestLayout>
     );
 }
--- resources/js/Pages/Auth/ResetPassword.tsx
+++ resources/js/Pages/Auth/ResetPassword.tsx
@@ -1,92 +1,12 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import ChangePasswordForm from "@/Components/forms/ChangePasswordForm";
 import GuestLayout from "@/Layouts/GuestLayout";
-import InputError from "@/Components/InputError";
-import Spinner from "@/Components/Spinner";
-import PrimaryButton from "@/Components/PrimaryButton";
-import { TextInput } from "@/Components/inputs/TextInput";
 import { Head } from "@inertiajs/react";
-type Inputs = {
-    password: string;
-    confirmation: string;
-};
 
 export default function ResetPassword() {
-    const [errorMessage, setErrorMessage] = useState("");
-    const [processing, setProcessing] = useState(false);
-
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm<Inputs>();
-
-    const submit: SubmitHandler<Inputs> = async (data) => {
-        try {
-            setErrorMessage("");
-            setProcessing(true);
-            await axios.post(route("password.update", data));
-            window.location.replace(route("dashboard"));
-        } catch (error: any) {
-            setProcessing(false);
-            setErrorMessage(error.response.data.message);
-            reset();
-        }
-    };
-
     return (
         <GuestLayout>
             <Head title="Reset Password" />
-
-            <form onSubmit={handleSubmit(submit)}>
-                <div className="mt-4">
-                    <TextInput
-                        label={"password"}
-                        labelText={"New Password"}
-                        interfaceRef={"password"}
-                        length={50}
-                        required={true}
-                        errors={errors}
-                        register={register}
-                        password={true}
-                        autoComplete="new-password"
-                        isFocused={true}
-                    />
-
-                    <div className="h-6">
-                        <InputError message={errorMessage} className="pt-2" />
-                    </div>
-                </div>
-
-                <div className="mt-4">
-                    <TextInput
-                        label={"confirm"}
-                        labelText={"Confirm Password"}
-                        interfaceRef={"password_confirmation"}
-                        length={50}
-                        required={true}
-                        errors={errors}
-                        register={register}
-                        password={true}
-                        autoComplete="new-password"
-                    />
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton
-                        className="ms-4 w-40 h-10"
-                        disabled={processing}
-                    >
-                        {processing ? (
-                            <Spinner extraClasses="m-auto" />
-                        ) : (
-                            <div className="m-auto">Reset Password</div>
-                        )}
-                    </PrimaryButton>
-                </div>
-            </form>
+            <ChangePasswordForm />
         </GuestLayout>
     );
 }
