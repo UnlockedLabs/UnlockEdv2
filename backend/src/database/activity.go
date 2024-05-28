00068cad2dff7dac0bdee4ea956270171dd492eb
PThorpe92 <preston@unlockedlabs.org>
2024-05-28 16:28:59 -0400

    fix: add missing user dashboard field, dont return resource

--- backend/src/database/activity.go
+++ backend/src/database/activity.go
@@ -31,61 +31,17 @@ func (db *DB) DeleteActivity(activityID int) error {
 	return db.Conn.Delete(&models.Activity{}, activityID).Error
 }
 
-func (db *DB) GetUserDashboardInfo(userID int) (models.UserDashboardJoin, error) {
-	var recentPrograms [3]models.RecentProgram
-	// first get the users 3 most recently interacted with programs
-	//
-	// get the users 3 most recent programs. progress: # of milestones where type = "assignment_submission" && status = "complete" || "graded" +
-	// # of milestones where type = "quiz_submission" && status = "complete" || "graded"
-	// where the user has an entry in the activity table
-	err := db.Conn.Table("programs p").
-		Select(`p.name as program_name,
-        p.alt_name,
-        p.thumbnail_url,
-        pp.name as provider_platform_name,
-        COUNT(sub_milestones.id) * 100.0 / p.total_progress_milestones as course_progress`).
-		Joins(`LEFT JOIN (
-    SELECT id, program_id FROM milestones
-    WHERE user_id = ? AND type IN ('assignment_submission', 'quiz_submission') AND is_completed = true
-    ORDER BY created_at DESC
-    LIMIT 3 ) sub_milestones ON sub_milestones.program_id = p.id`, userID).
-		Joins("LEFT JOIN provider_platforms pp ON p.provider_platform_id = pp.id").
-		Where("p.id IN (SELECT program_id FROM activities WHERE user_id = ?)", userID).
-		Group("p.id, p.name, p.alt_name, p.thumbnail_url, pp.name").
-		Find(&recentPrograms).Error
-	if err != nil {
-		log.Errorf("Error getting recent programs: %v", err)
-		recentPrograms = [3]models.RecentProgram{}
-	}
-	// then get the users current enrollments
-	// which is the program.alt_name, program.name, provider_platform.name as provider_platform_name (join on provider_platform_id = provider_platform.id), program.external_url,
-	// and acitvity.total_activity_time for the last 7 days of activity where activity.user_id = userID and activity.program_id = program.id
-	var results []struct {
-		ProgramID            uint
-		AltName              string
-		Name                 string
-		ProviderPlatformName string
-		ExternalURL          string
-		Date                 string
-		TimeDelta            uint
-	}
+type result struct {
+	ProgramID            uint
+	AltName              string
+	Name                 string
+	ProviderPlatformName string
+	ExternalURL          string
+	Date                 string
+	TimeDelta            uint
+}
 
-	err = db.Conn.Table("programs p").
-		Select(`p.id as program_id,
-				p.alt_name,
-				p.name,
-				pp.name as provider_platform_name,
-				p.external_url,
-				DATE(a.created_at) as date,
-				a.time_delta`).
-		Joins("JOIN provider_platforms pp ON p.provider_platform_id = pp.id").
-		Joins("JOIN activities a ON a.program_id = p.id").
-		Where("a.user_id = ? AND a.created_at >= ?", userID, time.Now().AddDate(0, 0, -7)).
-		Group("p.id, a.time_delta, DATE(a.created_at), pp.name").
-		Find(&results).Error
-	if err != nil {
-		log.Fatalf("Query failed: %v", err)
-	}
+func dashboardHelper(results []result) []models.CurrentEnrollment {
 	enrollmentsMap := make(map[uint]*models.CurrentEnrollment)
 	for _, result := range results {
 		if _, exists := enrollmentsMap[result.ProgramID]; !exists {
@@ -110,6 +66,58 @@ func (db *DB) GetUserDashboardInfo(userID int) (models.UserDashboardJoin, error)
 	for _, enrollment := range enrollmentsMap {
 		enrollments = append(enrollments, *enrollment)
 	}
+	return enrollments
+}
+
+func (db *DB) GetUserDashboardInfo(userID int) (models.UserDashboardJoin, error) {
+	var recentPrograms [3]models.RecentProgram
+	// first get the users 3 most recently interacted with programs
+	//
+	// get the users 3 most recent programs. progress: # of milestones where type = "assignment_submission" && status = "complete" || "graded" +
+	// # of milestones where type = "quiz_submission" && status = "complete" || "graded"
+	// where the user has an entry in the activity table
+	err := db.Conn.Table("programs p").
+		Select(`p.name as program_name,
+        p.alt_name,
+        p.thumbnail_url,
+        p.external_url,
+        pp.name as provider_platform_name,
+        COUNT(sub_milestones.id) * 100.0 / p.total_progress_milestones as course_progress`).
+		Joins(`LEFT JOIN (
+    SELECT id, program_id FROM milestones
+    WHERE user_id = ? AND type IN ('assignment_submission', 'quiz_submission') AND is_completed = true
+    ORDER BY created_at DESC
+    LIMIT 3 ) sub_milestones ON sub_milestones.program_id = p.id`, userID).
+		Joins("LEFT JOIN provider_platforms pp ON p.provider_platform_id = pp.id").
+		Where("p.id IN (SELECT program_id FROM activities WHERE user_id = ?)", userID).
+		Group("p.id, p.name, p.alt_name, p.thumbnail_url, pp.name").
+		Find(&recentPrograms).Error
+	if err != nil {
+		log.Errorf("Error getting recent programs: %v", err)
+		recentPrograms = [3]models.RecentProgram{}
+	}
+	// then get the users current enrollments
+	// which is the program.alt_name, program.name, provider_platform.name as provider_platform_name (join on provider_platform_id = provider_platform.id), program.external_url,
+	// and acitvity.total_activity_time for the last 7 days of activity where activity.user_id = userID and activity.program_id = program.id
+	results := []result{}
+
+	err = db.Conn.Table("programs p").
+		Select(`p.id as program_id,
+				p.alt_name,
+				p.name,
+				pp.name as provider_platform_name,
+				p.external_url,
+				DATE(a.created_at) as date,
+				a.time_delta`).
+		Joins("JOIN provider_platforms pp ON p.provider_platform_id = pp.id").
+		Joins("JOIN activities a ON a.program_id = p.id").
+		Where("a.user_id = ? AND a.created_at >= ?", userID, time.Now().AddDate(0, 0, -7)).
+		Group("p.id, a.time_delta, DATE(a.created_at), pp.name").
+		Find(&results).Error
+	if err != nil {
+		log.Fatalf("Query failed: %v", err)
+	}
+	enrollments := dashboardHelper(results)
 	if len(enrollments) == 0 {
 		log.Println("No enrollments found")
 		var newEnrollments []struct {
