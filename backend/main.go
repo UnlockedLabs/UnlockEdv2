3c61d2ed138592071d942e2b632350d0df86a73a
PThorpe92 <preston@unlockedlabs.org>
2024-05-31 14:49:46 -0400

    refactor: move initLogging out of main, set log level

--- backend/main.go
+++ backend/main.go
@@ -16,31 +16,48 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Info("no .env file found, using default env variables")
 	}
-	var file *os.File
-	var err error
+	env := os.Getenv("APP_ENV")
 	port := os.Getenv("APP_PORT")
 	if port == "" {
 		port = "8080"
 	}
-	env := os.Getenv("APP_ENV")
 	testing := (env == "testing")
-	prod := (env == "prod" || env == "production")
-	if prod {
-		file, err = os.OpenFile("logs/server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatalf("Failed to open log file: %v", err)
-		}
-		log.SetFormatter(&log.JSONFormatter{})
-	} else {
-		file = os.Stdout
-		log.SetFormatter(&log.TextFormatter{ForceColors: true})
-	}
-	defer file.Close()
-	log.SetOutput(file)
+	initLogging(env)
 	newServer := server.NewServer(testing)
 	log.Info("Starting server on :", port)
 	fmt.Println("Starting server on :", port)
 	if err := http.ListenAndServe(":8080", server.CorsMiddleware(newServer.Mux)); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
+}
+
+func initLogging(env string) {
+	var file *os.File
+	var err error
+	prod := (env == "prod" || env == "production")
+	logLevel := os.Getenv("LOG_LEVEL")
+	if prod {
+		file, err = os.OpenFile("logs/server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("Failed to open log file: %v", err)
+		}
+		if logLevel == "" {
+			logLevel = "info"
+		}
+		log.SetFormatter(&log.JSONFormatter{})
+	} else {
+		if logLevel == "" {
+			logLevel = "debug"
+		}
+		file = os.Stdout
+		log.SetFormatter(&log.TextFormatter{ForceColors: true})
+	}
+	defer file.Close()
+	level, err := log.ParseLevel(logLevel)
+	if err != nil {
+		log.Errorf("Error parsing log level: %v", err)
+		level = log.InfoLevel
+	}
+	log.SetLevel(level)
+	log.SetOutput(file)
 }
