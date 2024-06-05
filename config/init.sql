7c47fa575265fa45ca21bcccb1f78883167ebaf6
PThorpe92 <preston@unlockedlabs.org>
2024-06-05 13:43:13 -0400

    feat: ory configuration mgmt

--- config/init.sql
+++ config/init.sql
@@ -1,11 +1,17 @@
 CREATE USER hydra WITH PASSWORD 'ChangeMe!';
 CREATE DATABASE hydra;
 GRANT ALL PRIVILEGES ON DATABASE hydra TO hydra;
+\c hydra
+GRANT ALL ON SCHEMA public TO hydra;
 
 CREATE USER kratos WITH PASSWORD 'ChangeMe!';
 CREATE DATABASE kratos;
 GRANT ALL PRIVILEGES ON DATABASE kratos TO kratos;
+\c kratos
+GRANT ALL ON SCHEMA public TO kratos;
 
 CREATE USER keto WITH PASSWORD 'ChangeMe!';
-CREATE DATABASE keto;
-GRANT ALL PRIVILEGES ON DATABASE keto TO keto;
+CREATE DATABASE accesscontroldb;
+GRANT ALL PRIVILEGES ON DATABASE accesscontroldb TO keto;
+\c accesscontroldb
+GRANT ALL ON SCHEMA public TO keto;
