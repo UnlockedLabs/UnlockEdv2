cfb78323abba0e851026e9d4e17de4542cdeccd9
calisio <calisio@wustl.edu>
2024-07-16 21:12:15 -0400

    fix: colors on login with theme

--- frontend/src/Pages/Auth/Login.tsx
+++ frontend/src/Pages/Auth/Login.tsx
@@ -13,7 +13,7 @@ export default function Login({ status }: { status?: string }) {
       <div title="Log in" />
       <GuestLayout>
         {status && (
-          <div className="mb-4 font-medium text-sm text-green-600">
+          <div className="mb-4 font-medium text-sm text-body-text bg-background">
             {status}
           </div>
         )}
