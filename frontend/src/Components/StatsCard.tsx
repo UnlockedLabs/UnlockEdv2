5779308b2c1118c9caa77af7df491fb45a752c5b
calisio <calisio@wustl.edu>
2024-07-17 20:49:49 -0400

    fix: fixes stats card spacing & card overflow

--- frontend/src/Components/StatsCard.tsx
+++ frontend/src/Components/StatsCard.tsx
@@ -8,8 +8,8 @@ export default function StatsCard({
   label: string;
 }) {
   return (
-    <div className="card bg-base-teal p-4 pb-5">
-      <h3 className="text-teal-4">{title.toUpperCase()}</h3>
+    <div className="card bg-base-teal p-4 pb-5 flex flex-col justify-between">
+      <h3 className="text-teal-4 line-clamp-2">{title.toUpperCase()}</h3>
       <p className="text-teal-3 text-4xl font-bold mt-4">
         {number}
         <span className="text-teal-4 text-base font-bold ml-3">
