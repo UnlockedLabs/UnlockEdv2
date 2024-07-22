855561366dc75c72b7d7372e5d7cd1bcd0aa02f8
calisio <calisio@wustl.edu>
2024-07-22 07:41:13 -0400

    fix: student dash enrolled courses truncates alt name

--- frontend/src/Components/CurrentlyEnrolledClass.tsx
+++ frontend/src/Components/CurrentlyEnrolledClass.tsx
@@ -1,13 +1,19 @@
 import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
 
 export default function CurrentlyEnrolledClass({ course }: { course: any }) {
+  const truncateAltName = (altName: string) => {
+    if (altName.length < 15) return altName;
+    return altName.slice(0, 15) + "...";
+  };
   return (
     <div
       className={`card bg-inner-background flex flex-row justify-between body-small py-3 px-6`}
     >
       <div className="flex flex-row">
         {course.alt_name !== "" && (
-          <p className="font-bold w-[100px]">{course.alt_name}</p>
+          <p className="font-bold w-[150px]">
+            {truncateAltName(course.alt_name)}
+          </p>
         )}
         <p>{course.name}</p>
       </div>
