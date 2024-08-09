11679f12ae7529f42d7c66c01e38a8bfc2a108cc
PThorpe92 <preston@unlockedlabs.org>
2024-08-08 23:00:02 -0400

    fix: replace redirect to kratos when no flow is found in /login

--- frontend/src/Pages/Auth/Login.tsx
+++ frontend/src/Pages/Auth/Login.tsx
@@ -1,6 +1,11 @@
 import LoginForm from '@/Components/forms/LoginForm';
 import GuestLayout from '@/Layouts/GuestLayout';
+import { BROWSER_URL } from '@/common';
+
 export default function Login({ status }: { status?: string }) {
+    if (!window.location.search.includes('flow')) {
+        window.location.href = BROWSER_URL;
+    }
     return (
         <>
             <div title="Log in" />
