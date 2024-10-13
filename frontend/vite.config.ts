583f535921fe739176cd0644bd042f59ccd040f9
Huijiro <huijirohankei@gmail.com>
2024-10-13 14:48:53 -0300

    build: fixed build process

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: { sourcemap: true }, // we keep the sourcemap for now, so we can debug in staging
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        proxy: {
-            '/api': {
-                target: 'http://127.0.0.1',
-                changeOrigin: true
-            },
-            '/self-service': {
-                target: 'http://127.0.0.1',
-                changeOrigin: true
-            }
-        }
-    }
+  plugins: [react()],
+  build: { sourcemap: true }, // we keep the sourcemap for now, so we can debug in staging
 });
