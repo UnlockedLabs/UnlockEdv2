72aef13bb30bbc763eb5389de5172411552db463
MillerSenior <anthony.millersr82@gmail.com>
2024-10-15 12:23:36 -0500

    fix: removed grade from ui

--- frontend/src/Pages/MyProgress.tsx
+++ frontend/src/Pages/MyProgress.tsx
@@ -110,7 +110,7 @@ export default function MyProgress() {
                                                 Course Name
                                             </th>
                                             <th className="w-1/5">Status</th>
-                                            <th className="w-1/5">Grade</th>
+                                            {/*<th className="w-1/5">Grade</th>*/}
                                             <th className="w-1/5">
                                                 Hours Spent
                                             </th>
@@ -158,10 +158,10 @@ export default function MyProgress() {
                                                                 </TealPill>
                                                             )}
                                                         </td>
-                                                        <td className="w-1/5">
-                                                            {course?.grade ||
-                                                                '-'}
-                                                        </td>
+                                                        {/*<td className="w-1/5">*/}
+                                                        {/*    {course?.grade ||*/}
+                                                        {/*        '-'}*/}
+                                                        {/*</td>*/}
                                                         <td className="w-1/5">
                                                             {courseTotalTime.number +
                                                                 ' ' +
