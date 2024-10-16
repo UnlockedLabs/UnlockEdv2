8c6e7647ed73021d9e9bdbe3dedc910851bcd997
MillerSenior <anthony.millersr82@gmail.com>
2024-10-16 17:01:23 -0500

    feat: added headers to all pages with px-8

--- frontend/src/Pages/StudentManagement.tsx
+++ frontend/src/Pages/StudentManagement.tsx
@@ -148,7 +148,7 @@ export default function StudentManagement() {
     };
     return (
         <div>
-            <div className="flex flex-col space-y-6 overflow-x-auto rounded-lg p-4">
+            <div className="flex flex-col space-y-6 overflow-x-auto rounded-lg p-4 px-8">
                 <h1>Student Management</h1>
                 <div className="flex justify-between">
                     <div className="flex flex-row gap-x-2">
