ea71efedbcf73b9b700297108954faf0b712e0aa
cpride <cory.pride83@gmail.com>
2024-11-04 18:17:59 -0600

    refactor: modified add_facilities_programs migrations to include program_type field

--- backend/migrations/00015_add_facilities_programs_relationship.sql
+++ backend/migrations/00015_add_facilities_programs_relationship.sql
@@ -1,6 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
-alter table public.programs add COLUMN credit_type character varying(255), add COLUMN program_status character varying(50);
+alter table public.programs 
+add COLUMN credit_type character varying(255), 
+add COLUMN program_status character varying(50), 
+add COLUMN program_type character varying(50);
+
 create table public.facilities_programs(
     id SERIAL not null primary key,
     facility_id integer not null,
@@ -17,6 +21,9 @@ create index idx_facility_program_program_id on public.facilities_programs using
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
-alter table public.programs drop COLUMN credit_type, drop COLUMN program_status;
+alter table public.programs 
+drop COLUMN credit_type, 
+drop COLUMN program_status,
+drop COLUMN program_type;
 drop table if exists public.facilities_programs CASCADE;
 -- +goose StatementEnd
