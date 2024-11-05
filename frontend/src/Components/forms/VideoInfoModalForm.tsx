0b1f4cff145faffbe14be3c7221998e9be40274d
PThorpe92 <preston@unlockedlabs.org>
2024-11-05 11:14:34 -0500

    feat: add manual retry to video downloads

---
+++ frontend/src/Components/forms/VideoInfoModalForm.tsx
@@ -0,0 +1,47 @@
+import {
+    getVideoErrorMessage,
+    MAX_DOWNLOAD_ATTEMPTS,
+    UserRole,
+    Video
+} from '@/common';
+import { CloseX } from '../inputs';
+import { useAuth } from '@/useAuth';
+
+export interface VideoErrorFormProps {
+    video: Video;
+    onClose: () => void;
+}
+
+export default function VideoInfoModalForm({
+    video,
+    onClose
+}: VideoErrorFormProps) {
+    const { user } = useAuth();
+    if (!user || user?.role !== UserRole.Admin) {
+        return null;
+    }
+
+    const prepError = () => {
+        return video.video_download_attempts.length >= MAX_DOWNLOAD_ATTEMPTS
+            ? `This video has reached the maximum download attempts. Please remove and try again`
+            : `Download is processsing: ${getVideoErrorMessage(video) ?? ''}
+               The video download will be retried every 3 hours`;
+    };
+
+    return (
+        <div>
+            <CloseX close={() => onClose()} />
+            <div className="flex flex-row">
+                <div className="stats shadow mx-auto">
+                    <div className="stat">
+                        <div className="stat-title">Video download info</div>
+                        <div className="pt-5 text-sm text-wrap">
+                            {prepError()}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p className="py-4"></p>
+        </div>
+    );
+}
--- provider-middleware/video_dl.go
+++ provider-middleware/video_dl.go
@@ -15,7 +15,6 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/sirupsen/logrus"
 	"github.com/wader/goutubedl"
 	"gorm.io/gorm"
 )
@@ -82,7 +81,7 @@ func NewVideoService(prov *models.OpenContentProvider, db *gorm.DB, body *map[st
 func (yt *VideoService) downloadAndHostThumbnail(yt_id, url string) (string, error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		logrus.Errorf("error creating request: %v", err)
+		logger().Errorf("error creating request: %v", err)
 		return "", err
 	}
 	req.Header.Set("User-Agent", "Mozilla/5.0")
@@ -90,18 +89,18 @@ func (yt *VideoService) downloadAndHostThumbnail(yt_id, url string) (string, err
 
 	resp, err := yt.Client.Do(req)
 	if err != nil {
-		logrus.Errorf("error fetching thumbnail image from URL: %v", err)
+		logger().Errorf("error fetching thumbnail image from URL: %v", err)
 		return "", err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		logrus.Errorf("failed to fetch thumbnail image: received %v response", resp.Status)
+		logger().Errorf("failed to fetch thumbnail image: received %v response", resp.Status)
 		return "", fmt.Errorf("failed to fetch thumbnail image: %v", resp.Status)
 	}
 
 	imgData, err := io.ReadAll(resp.Body)
 	if err != nil {
-		logrus.Errorf("error reading thumbnail image: %v", err)
+		logger().Errorf("error reading thumbnail image: %v", err)
 		return "", err
 	}
 
@@ -126,26 +125,26 @@ func (yt *VideoService) downloadAndHostThumbnail(yt_id, url string) (string, err
 	}
 	err = writer.Close()
 	if err != nil {
-		logrus.Errorf("error closing writer: %v", err)
+		logger().Errorf("error closing writer: %v", err)
 		return "", err
 	}
 
 	uploadURL := os.Getenv("APP_URL") + "/upload"
 	req, err = http.NewRequest(http.MethodPost, uploadURL, body)
 	if err != nil {
-		logrus.Errorf("error creating upload request: %v", err)
+		logger().Errorf("error creating upload request: %v", err)
 		return "", err
 	}
 	req.Header.Set("Content-Type", writer.FormDataContentType())
 
 	uploadResp, err := yt.Client.Do(req)
 	if err != nil {
-		logrus.Errorf("error sending upload request: %v", err)
+		logger().Errorf("error sending upload request: %v", err)
 		return "", err
 	}
 	defer uploadResp.Body.Close()
 	if uploadResp.StatusCode != http.StatusOK {
-		logrus.Errorf("failed to upload image: received %v response", uploadResp.Status)
+		logger().Errorf("failed to upload image: received %v response", uploadResp.Status)
 		return "", fmt.Errorf("failed to upload image: %v", uploadResp.Status)
 	}
 
@@ -159,7 +158,7 @@ func (yt *VideoService) downloadAndHostThumbnail(yt_id, url string) (string, err
 	urlRes := &UploadResponse{}
 	err = json.NewDecoder(uploadResp.Body).Decode(urlRes)
 	if err != nil {
-		logrus.Errorf("error decoding upload response: %v", err)
+		logger().Errorf("error decoding upload response: %v", err)
 		return "", err
 	}
 	return urlRes.Data.URL, nil
@@ -183,25 +182,41 @@ func stripUrlsFromDescription(desc string) string {
 
 func (vs *VideoService) RetryFailedVideos(ctx context.Context) error {
 	videos := make([]models.Video, 0)
-	if err := vs.db.WithContext(ctx).Find(&videos, "is_available = false").Error; err != nil {
-		logrus.Errorf("error fetching failed videos: %v", err)
+	if err := vs.db.WithContext(ctx).Model(&models.Video{}).Find(&videos, "availability <> 'available'").Error; err != nil {
+		logger().Errorf("error fetching failed videos: %v", err)
 	}
 	for _, video := range videos {
-		if video.Url == nil {
-			if video.YoutubeID == nil {
-				logrus.Errorf("video has no url or youtube id, cannot proceed with download")
-				if err := vs.incrementFailedAttempt(ctx, &video, "video has no url or youtube id"); err != nil {
-					logrus.Errorf("error incrementing failed attempt: %v", err)
-				}
-				continue
-			} else {
-				video.Url = models.StringPtr(fmt.Sprintf("https://www.youtube.com/watch?v=%s", *video.YoutubeID))
+		if err := vs.RetrySingleVideo(ctx, int(video.ID)); err != nil {
+			logger().Errorf("error retrying single video: %v", err)
+			err = vs.incrementFailedAttempt(ctx, &video, err.Error())
+			if err != nil {
+				return err
 			}
 		}
-		err := vs.downloadVideo(ctx, *video.Url, &video)
-		if err != nil {
-			logrus.Errorf("error downloading video: %v", err)
+	}
+	return nil
+}
+
+func (vs *VideoService) RetrySingleVideo(ctx context.Context, videoId int) error {
+	var video models.Video
+	if err := vs.db.WithContext(ctx).First(&video, videoId).Error; err != nil {
+		logger().Errorf("error fetching video: %v", err)
+		return vs.incrementFailedAttempt(ctx, &video, err.Error())
+	}
+	if video.Url == nil {
+		if video.YoutubeID == nil {
+			logger().Errorf("video has no url or youtube id, cannot proceed with download")
+			if err := vs.incrementFailedAttempt(ctx, &video, "video has no url or youtube id"); err != nil {
+				logger().Errorf("error incrementing failed attempt: %v", err)
+			}
+		} else {
+			video.Url = models.StringPtr(fmt.Sprintf("https://www.youtube.com/watch?v=%s", *video.YoutubeID))
 		}
+	}
+	err := vs.downloadVideo(ctx, *video.Url, &video)
+	if err != nil {
+		logger().Errorf("error downloading video: %v", err)
+		vs.incrementFailedAttempt(ctx, &video, err.Error())
 	}
 	return nil
 }
@@ -209,7 +224,7 @@ func (vs *VideoService) RetryFailedVideos(ctx context.Context) error {
 func (yt *VideoService) AddVideos(ctx context.Context) error {
 	params := *yt.Body
 	urls := params["video_urls"].([]interface{})
-	logrus.Infof("Adding videos: %v", urls)
+	logger().Infof("Adding videos: %v", urls)
 	for idx := range urls {
 		select {
 		case <-ctx.Done():
@@ -219,12 +234,12 @@ func (yt *VideoService) AddVideos(ctx context.Context) error {
 			if strings.Contains(urlStr, "youtube.com") {
 				err := yt.fetchYoutubeInfo(ctx, urlStr)
 				if err != nil {
-					logrus.Errorf("Error fetching youtube info: %v", err)
+					logger().Errorf("Error fetching youtube info: %v", err)
 				}
 			} else {
 				err := yt.downloadVideo(ctx, urlStr, nil)
 				if err != nil {
-					logrus.Errorf("Error downloading video: %v", err)
+					logger().Errorf("Error downloading video: %v", err)
 				}
 			}
 		}
@@ -235,31 +250,31 @@ func (yt *VideoService) AddVideos(ctx context.Context) error {
 func (vs *VideoService) fetchYoutubeInfo(ctx context.Context, ytUrl string) error {
 	parsed, err := url.Parse(ytUrl)
 	if err != nil {
-		logrus.Errorf("Error parsing url: %v", err)
+		logger().Errorf("Error parsing url: %v", err)
 		return err
 	}
 	id := parsed.Query().Get("v")
 	finalUrl := fmt.Sprintf("%s?id=%s&key=%s%s", vs.BaseUrl, id, vs.YtApiKey, YtQueryParams)
-	logrus.Infof("url to fetch video: %s", finalUrl)
+	logger().Infof("url to fetch video: %s", finalUrl)
 	resp, err := http.Get(finalUrl)
 	if err != nil {
-		logrus.Errorf("Error getting video: %v", err)
+		logger().Errorf("Error getting video: %v", err)
 		return err
 	}
 	defer resp.Body.Close()
 	var data YoutubeDataResponse
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		logrus.Errorf("Error decoding video info: %v", err)
+		logger().Errorf("Error decoding video info: %v", err)
 		return err
 	}
 	if len(data.Items) == 0 {
-		logrus.Errorf("Video not found in response from youtube API")
+		logger().Errorf("Video not found in response from youtube API")
 		return fmt.Errorf("Video not found")
 	}
-	logrus.Infof("Adding videos: %v", data)
+	logger().Infof("Adding videos: %v", data)
 	thumbnailUrl, err := vs.downloadAndHostThumbnail(data.Items[0].ID, data.Items[0].Snippet.Thumbnails.High.Url)
 	if err != nil {
-		logrus.Errorf("Error downloading and hosting thumbnail: %v", err)
+		logger().Errorf("Error downloading and hosting thumbnail: %v", err)
 		thumbnailUrl = ""
 	}
 	vid := &models.Video{
@@ -270,16 +285,16 @@ func (vs *VideoService) fetchYoutubeInfo(ctx context.Context, ytUrl string) erro
 		ChannelTitle:          &data.Items[0].Snippet.ChannelTitle,
 		Availability:          models.VideoProcessing,
 		ThumbnailUrl:          thumbnailUrl,
-		VisibilityStatus:      true,
+		VisibilityStatus:      false,
 		OpenContentProviderID: vs.OpenContentProviderID,
 	}
 	err = vs.db.WithContext(ctx).Create(vid).Error
 	if err != nil {
-		logrus.Errorf("Error creating video: %v", err)
+		logger().Errorf("Error creating video: %v", err)
 	}
 	err = vs.downloadVideo(ctx, ytUrl, vid)
 	if err != nil {
-		logrus.Errorf("Error updating video: %v", err)
+		logger().Errorf("Error updating video: %v", err)
 	}
 	return nil
 }
@@ -287,61 +302,68 @@ func (vs *VideoService) fetchYoutubeInfo(ctx context.Context, ytUrl string) erro
 func (yt *VideoService) incrementFailedAttempt(ctx context.Context, vid *models.Video, vidError string) error {
 	var numAttempts int64
 	if err := yt.db.WithContext(ctx).Model(&models.VideoDownloadAttempt{}).Where("video_id = ?", vid.ID).Count(&numAttempts).Error; err != nil {
-		logrus.Errorf("error counting download attempts: %v", err)
+		logger().Errorf("error counting download attempts: %v", err)
 		return err
 	}
 	if numAttempts >= int64(MAX_DOWNLOAD_ATTEMPTS()) || vidError != "" {
 		vid.Availability = models.VideoHasError
 		if err := yt.db.WithContext(ctx).Save(vid).Error; err != nil {
-			logrus.Errorf("error updating video: %v", err)
+			logger().Errorf("error updating video: %v", err)
 			return err
 		}
 	}
 	attempt := models.VideoDownloadAttempt{
-		VideoID: vid.ID,
-		Error:   vidError,
+		VideoID:      vid.ID,
+		ErrorMessage: vidError,
 	}
 	return yt.db.WithContext(ctx).Create(&attempt).Error
 }
 
 func (yt *VideoService) downloadVideo(ctx context.Context, url string, vid *models.Video) error {
-	result, err := goutubedl.New(context.Background(), url, goutubedl.Options{
+	opts := goutubedl.Options{
 		Type:     goutubedl.TypeAny,
-		DebugLog: logrus.New(),
-		ProxyUrl: "socks5://127.0.0.1:25432",
-	})
+		DebugLog: logger(),
+	}
+	result, err := goutubedl.New(ctx, url, opts)
 	if err != nil {
-		logrus.Errorf("error initiaing yt-dlp: %v", err)
+		logger().Errorf("error initiaing yt-dlp: %v", err)
 		return err
 	}
-	logrus.Println("info: ", result.Info)
+	logger().Println("info: ", result.Info)
 	downloadResult, err := result.Download(ctx, "best")
 	if err != nil {
-		logrus.Errorf("error downloading yt-dlp: %v", err)
+		logger().Errorf("error downloading yt-dlp: %v", err)
 		return err
 	}
 	defer downloadResult.Close()
 	if vid == nil {
+		thumbnail, err := yt.downloadAndHostThumbnail(result.Info.ID, result.Info.Thumbnail)
+		if err != nil {
+			thumbnail = "/youtube.png"
+		}
 		vid = &models.Video{
 			Url:                   &url,
 			Title:                 result.Info.Title,
 			Description:           result.Info.Description,
 			ChannelTitle:          &result.Info.Channel,
+			Availability:          models.VideoProcessing,
+			VisibilityStatus:      false,
+			ThumbnailUrl:          thumbnail,
 			OpenContentProviderID: yt.OpenContentProviderID,
 		}
 		if err := yt.db.WithContext(ctx).Create(vid).Error; err != nil {
-			logrus.Errorln(err)
+			logger().Errorln(err)
 		}
 	}
 	file, err := os.Create(fmt.Sprintf("%s/%s.mp4", os.Getenv("VIDEO_DOWNLOAD_DIR"), result.Info.ID))
 	if err != nil {
-		logrus.Error(err)
+		logger().Error(err)
 		return err
 	}
 	defer file.Close()
 	_, err = io.Copy(file, downloadResult)
 	if err != nil {
-		logrus.Errorf("error: %v copying downloaded video result", err)
+		logger().Errorf("error: %v copying downloaded video result", err)
 		return err
 	}
 	vid.Availability = models.VideoAvailable
