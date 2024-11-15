80c76783c2b99bd81fcf98e8bc742e72b8faeefe
Richard Salas <rs.timothyjr@gmail.com>
2024-11-15 13:33:16 -0600

    feat: impl import activity for course for BrightspaceService
    * fix: move drop and create statements to proper locations
    
    * feat: add logic for the integration of brightspace activities for courses and add logic to SQL function for handling total times and time deltas specific to brightspace
    
    * fix: modify Error check to be RowsAffected check

--- backend/migrations/00017_drop_create_insert_daily_activity_sql.sql
+++ backend/migrations/00017_drop_create_insert_daily_activity_sql.sql
@@ -13,6 +13,7 @@ language plpgsql
 as 
 '
     DECLARE prev_total_time INT;
+    DECLARE prev_delta_time INT;
     DECLARE provider_type CHAR ( 100 );
     BEGIN
         select p.type INTO provider_type from activities a 
@@ -25,14 +26,31 @@ as
         SELECT total_time INTO prev_total_time FROM activities
         WHERE user_id = _user_id AND course_id = _course_id
         ORDER BY created_at DESC LIMIT 1;
+
         IF prev_total_time IS NULL THEN
             prev_total_time := 0;
         END IF;
+        --brightspace previous time_delta is needed for recalulating actual
+        --total time since brightspace is accumulating time spent on 
+        --possibly the same content until student has completed content
+        IF prev_total_time > 0 AND provider_type = ''brightspace'' THEN
+                SELECT time_delta INTO prev_delta_time FROM activities
+                WHERE user_id = _user_id AND course_id = _course_id
+                        AND SUBSTRING(external_id FROM ''^[^-]+'') = SUBSTRING(_external_id FROM ''^[^-]+'')
+                ORDER BY created_at DESC LIMIT 1;
+        END IF;
+        
+        IF prev_delta_time IS NULL THEN
+            prev_delta_time := 0;
+        END IF;
         --kolibri total_time is the delta and total_time 
-        --is required to be added to previous_total_time 
+        --is required to be added to previous_total_time
         IF provider_type = ''kolibri'' THEN
                 INSERT INTO activities (user_id, course_id, type, total_time, time_delta, external_id, created_at, updated_at)
                 VALUES (_user_id, _course_id, _type, _total_time + prev_total_time, _total_time, _external_id, NOW(), NOW());
+        ELSEIF provider_type = ''brightspace'' THEN
+                INSERT INTO activities (user_id, course_id, type, total_time, time_delta, external_id, created_at, updated_at)
+                VALUES (_user_id, _course_id, _type, (_total_time-prev_delta_time)+prev_total_time, _total_time-prev_delta_time, _external_id, NOW(), NOW());
         ELSE
                 INSERT INTO activities (user_id, course_id, type, total_time, time_delta, external_id, created_at, updated_at)
                 VALUES (_user_id, _course_id, _type, _total_time, _total_time - prev_total_time, _external_id, NOW(), NOW());
