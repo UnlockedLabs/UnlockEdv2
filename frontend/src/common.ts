87c9f9572699422bc341957f69f50fdd0667147a
CK-7vn <keighan.robichaudgriffin@maine.edu>
2024-12-03 11:52:23 -0500

    feat: add backend api calls for helpful-links and some frontend work

--- frontend/src/common.ts
+++ frontend/src/common.ts
@@ -658,7 +658,7 @@ export interface Resource {
 
 export interface HelpfulLink {
     id: number;
-    name: string;
+    title: string;
     description: string;
     url: string;
     visibility_status: boolean;
