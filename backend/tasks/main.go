6b975bf8728d2dc9eb220471bea85ea4a73d14dd
CK-7vn <keighan.robichaudgriffin@maine.edu>
2024-12-10 12:15:20 -0500

    feat: add cron scheduling for Brightspace

--- backend/tasks/main.go
+++ backend/tasks/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"UnlockEdv2/src/models"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -27,15 +29,20 @@ func main() {
 		log.Fatalf("failed to generate tasks: %v", err)
 		return
 	}
+	hour := 1
 	for _, task := range tasks {
 		if task.Job == nil {
 			log.Errorf("Task %v has no job", task.ID)
 			continue
 		}
-		_, err := scheduler.NewJob(gocron.CronJob(task.Job.Schedule, false), gocron.NewTask(runner.runTask, task))
+		_, err := scheduler.NewJob(gocron.CronJob(getCronSchedule(&task, hour), false), gocron.NewTask(runner.runTask, &task))
 		if err != nil {
 			log.Errorf("Failed to create job: %v", err)
 			continue
+		}
+		hour++
+		if hour > 23 {
+			hour = 1
 		}
 	}
 	runner.execute()
@@ -67,4 +74,12 @@ func initLogging() {
 		level = log.DebugLevel
 	}
 	log.SetLevel(level)
+}
+
+func getCronSchedule(task *models.RunnableTask, hour int) string {
+	if task.Provider != nil && task.Provider.Type == models.Brightspace {
+		return fmt.Sprintf("0 0 %d * * 4", hour)
+	} else {
+		return task.Job.Schedule
+	}
 }
