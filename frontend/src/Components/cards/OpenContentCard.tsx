28b6fb67ecbc679822410a41cb7255116ff31ef5
carddev81 <rich@unlockedlabs.org>
2025-01-24 17:52:28 -0600

    fix: modify logic to disable favorited content items rather than hide them

--- frontend/src/Components/cards/OpenContentCard.tsx
+++ frontend/src/Components/cards/OpenContentCard.tsx
@@ -8,16 +8,19 @@ export default function OpenContentCardRow({
 }) {
     const navigate = useNavigate();
     function redirectToViewer() {
+        if (content.visibility_status != null && !content.visibility_status) return;
         const basePath =
             content.content_type === 'video'
                 ? `/viewer/videos/${content.content_id}`
                 : `/viewer/libraries/${content.content_id}`;
         navigate(basePath);
     }
-
     return (
         <div
-            className="card cursor-pointer flex flex-row w-full gap-3 px-4 py-2"
+            className={`card ${content.visibility_status == null || content.visibility_status ? 'cursor-pointer' : 'bg-grey-2 cursor-not-allowed'} flex flex-row w-full gap-3 px-4 py-2 tooltip`}
+            {...((content.visibility_status != null && !content.visibility_status) && {
+                'data-tip': 'This content is no longer accessible'
+            })}
             onClick={redirectToViewer}
         >
             <div className="w-[100px]">
