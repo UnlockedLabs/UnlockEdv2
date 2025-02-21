47d0afdd7326ac332b3f7fa665da0235c04e323a
CK-7vn <keighan.robichaudgriffin@maine.edu>
2025-02-20 23:52:27 -0500

    fix: remove admin ability to alter data in student view

--- frontend/src/Components/FavoriteCard.tsx
+++ frontend/src/Components/FavoriteCard.tsx
@@ -1,9 +1,11 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { StarIcon as SolidStar } from '@heroicons/react/24/solid';
 import { KeyedMutator } from 'swr';
 import { ToastState, ServerResponseMany, OpenContentItem } from '@/common';
 import API from '@/api/api';
 import { useToast } from '@/Context/ToastCtx';
+
+import { isAdministrator, useAuth } from '@/useAuth';
 
 interface FavoriteCardProps {
     favorite: OpenContentItem;
@@ -20,6 +22,11 @@ const FavoriteCard: React.FC<FavoriteCardProps> = ({
 }) => {
     const navigate = useNavigate();
     const { toaster } = useToast();
+    const route = useLocation();
+    const { user } = useAuth();
+    const adminWithStudentView = (): boolean => {
+        return !route.pathname.includes('management') && isAdministrator(user);
+    };
 
     const handleCardClick = () => {
         if (!favorite.visibility_status) {
@@ -61,6 +68,9 @@ const FavoriteCard: React.FC<FavoriteCardProps> = ({
             }
         }
         const response = await API.put(endpoint, payload);
+        if (adminWithStudentView()) {
+            return;
+        }
         if (response.success) {
             toaster(`Removed from favorites`, ToastState.success);
             await mutate();
