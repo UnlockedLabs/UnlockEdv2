347e047d479c614aeb675e2e8ea0fd6ebb96affc
carddev81 <rich@unlockedlabs.org>
2025-03-19 15:12:42 -0500

    fix: modify structs and seeder to conform to program database structure

--- backend/migrations/00041_add_remove_program_columns.sql
+++ backend/migrations/00041_add_remove_program_columns.sql
@@ -12,6 +12,8 @@ ALTER TABLE public.program_sections DROP COLUMN status;
 ALTER TABLE public.program_sections ADD COLUMN status section_status;
 ALTER TABLE public.program_sections DROP COLUMN is_active;
 ALTER TABLE public.programs DROP COLUMN program_status;
+ALTER TABLE public.programs_sections_history ADD COLUMN created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE public.program_completions ALTER COLUMN program_name SET NOT NULL;
 DROP TABLE IF EXISTS public.program_types CASCADE;
 DROP TYPE IF EXISTS public.program_type;
 
@@ -41,6 +43,8 @@ ALTER TABLE public.program_sections ADD COLUMN status VARCHAR(50);
 ALTER TABLE public.program_sections ADD COLUMN is_active boolean;
 DROP TYPE IF EXISTS public.section_status;
 ALTER TABLE public.programs ADD COLUMN program_status VARCHAR(50);
+ALTER TABLE public.programs_sections_history DROP COLUMN created_at;
+ALTER TABLE public.program_completions ALTER COLUMN program_name DROP NOT NULL;
 DROP TABLE IF EXISTS public.program_types CASCADE;
 DROP TYPE IF EXISTS public.program_type;
 
