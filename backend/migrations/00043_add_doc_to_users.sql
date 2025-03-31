6d3c023a657c393f41a4489cf3f13c5a2f1b96b1
jtucholski <josh.tucholski@gmail.com>
2025-03-31 07:50:46 -0400

    fix: removed unnecessary migration

--- backend/migrations/00043_add_doc_to_users.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE public.users ADD COLUMN doc_id VARCHAR(32);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE public.users DROP COLUMN doc_id;
--- +goose StatementEnd
