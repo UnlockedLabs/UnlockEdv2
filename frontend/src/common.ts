cfea7456d2281fa69c0712961547b394d32dc6e8
CK-7vn <keighan.robichaudgriffin@maine.edu>
2025-04-09 14:32:04 -0400

    fix: fix void that caused issue with sending event attendance to back end

--- frontend/src/common.ts
+++ frontend/src/common.ts
@@ -713,9 +713,6 @@ export interface ClassEventInstance {
     date: string;
     attendance_records: ProgramClassEventAttendance[];
 }
-//export enum DateOrdering {
-//
-//}
 
 export interface ProgramClassEventAttendance {
     id: number;
@@ -737,12 +734,10 @@ export interface ProgramClassEnrollment {
 }
 export interface EnrollmentAttendance {
     enrollment: ProgramClassEnrollment;
-    attendance_status: string;
-    note: string;
     attendance: {
-        attendance_status: Attendance;
+        attendance_status: string;
         note: string;
-    } | null;
+    };
 }
 
 export enum Attendance {
