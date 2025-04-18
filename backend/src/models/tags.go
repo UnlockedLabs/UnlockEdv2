a43799a99de6a75e003da9ac8e240b4a2d8948c7
PThorpe92 <preston@unlockedlabs.org>
2025-04-18 17:06:34 -0400

    feat: add model for AuditHistory table

--- backend/src/models/tags.go
+++ backend/src/models/tags.go
@@ -1,8 +1,29 @@
 package models
 
+import "time"
+
 type Tag struct {
 	ID   uint   `gorm:"primaryKey" json:"key"`
 	Name string `gorm:"size:255;not null" json:"value"`
 }
 
 func (Tag) TableName() string { return "tags" }
+
+type AuditHistory struct {
+	ID         uint      `gorm:"primaryKey" json:"id"`
+	UserID     *uint     `json:"user_id"`
+	AdminID    *uint     `json:"admin_id"`
+	TableRef   string    `json:"table_ref"`
+	ColumnRef  string    `json:"column_ref"`
+	RefID      uint      `json:"ref_id"`
+	Action     string    `json:"action"`
+	Value      any       `json:"value"`
+	FacilityID *uint     `json:"facility_id"`
+	CreatedAt  time.Time `json:"created_at"`
+
+	User     *User     `json:"user" gorm:"foreignKey:UserID;references:ID"`
+	Admin    *User     `json:"admin" gorm:"foreignKey:AdminID;references:ID"`
+	Facility *Facility `json:"facility" gorm:"foreignKey:FacilityID;references:ID"`
+}
+
+func (AuditHistory) TableName() string { return "audit_history" }
