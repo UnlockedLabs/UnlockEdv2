cc4372e529c25fcfb8916823477198641c44a36a
calisio <carolina@unlockedlabs.org>
2025-04-28 12:44:26 -0400

    fix: add indexes to prog mgmt migrations, add default date

--- backend/migrations/00045_add_program_history.sql
+++ backend/migrations/00045_add_program_history.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 ALTER TABLE public.programs ADD COLUMN archived_at TIMESTAMP WITH TIME ZONE;
 CREATE TABLE public.daily_programs_facilities_history (
-    date DATE NOT NULL,
+    date DATE NOT NULL DEFAULT CURRENT_DATE,
     total_programs INTEGER NOT NULL,
     total_active_programs INTEGER NOT NULL,
     total_archived_programs INTEGER NOT NULL,
@@ -13,8 +13,9 @@ CREATE TABLE public.daily_programs_facilities_history (
     total_attendances_marked INTEGER NOT NULL,
     total_students_present INTEGER NOT NULL
 );
+CREATE INDEX idx_daily_programs_facilities_history_date ON public.daily_programs_facilities_history (date DESC);
 CREATE TABLE public.daily_program_facilities_history (
-    date DATE NOT NULL,
+    date DATE NOT NULL DEFAULT CURRENT_DATE,
     program_id INTEGER NOT NULL,
     total_active_facilities INTEGER NOT NULL,
     total_enrollments INTEGER NOT NULL,
@@ -25,8 +26,9 @@ CREATE TABLE public.daily_program_facilities_history (
     total_attendances_marked INTEGER NOT NULL,
     total_students_present INTEGER NOT NULL
 );
+CREATE INDEX idx_daily_programs_facilities_history_date_program_id ON public.daily_program_facilities_history (date DESC, program_id);
 CREATE TABLE public.daily_program_facility_history (
-    date DATE NOT NULL,
+    date DATE NOT NULL DEFAULT CURRENT_DATE,
     program_id INTEGER NOT NULL,
     facility_id INTEGER NOT NULL,
     total_enrollments INTEGER NOT NULL,
@@ -37,6 +39,8 @@ CREATE TABLE public.daily_program_facility_history (
     total_attendances_marked INTEGER NOT NULL,
     total_students_present INTEGER NOT NULL
 );
+CREATE INDEX idx_daily_programs_facilities_history_date ON public.daily_program_facility_history (date DESC, program_id, facility_id);
+
 -- +goose StatementEnd
 
 -- +goose Down
