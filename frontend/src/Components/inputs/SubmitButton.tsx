56c6f4e7e124d695b5167dc5e60fd16186c6c9ce
cpride <cory.pride83@gmail.com>
2025-04-30 06:57:36 -0500

    refactor: added isDisabled functionality to the doc_id input of the VerifyResidentModal

--- frontend/src/Components/inputs/SubmitButton.tsx
+++ frontend/src/Components/inputs/SubmitButton.tsx
@@ -1,14 +1,22 @@
 export function SubmitButton({
     errorMessage,
-    label = 'Submit'
+    label = 'Submit',
+    verifyResidentDOCIDMode,
+    isEnabled
 }: {
     errorMessage?: string;
     label?: string;
+    verifyResidentDOCIDMode?: boolean;
+    isEnabled?: boolean;
 }) {
     return (
         <label className="form-control pt-4">
-            <input className="btn btn-primary" type="submit" value={label} />
-
+            <input
+                className="btn btn-primary"
+                type="submit"
+                value={label}
+                disabled={verifyResidentDOCIDMode ? !isEnabled : false}
+            />
             {errorMessage ? (
                 <div className="text-error text-center pt-2">
                     {errorMessage}
