84c519cc37005fab97f5d86cbe0a3dd36d35b7a2
CK-7vn <keighan.robichaudgriffin@maine.edu>
2025-05-23 09:23:12 -0400

    fix: remove change reason migration

--- backend/migrations/00049_add_change_reason_to_class_enrollments.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE public.program_class_enrollments ADD COLUMN change_reason VARCHAR(255);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE public.program_class_enrollments DROP COLUMN change_reason;
--- +goose StatementEnd
