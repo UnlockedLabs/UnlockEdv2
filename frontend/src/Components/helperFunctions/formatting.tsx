91d688e5d771bc82cb8de870a422db16d9947522
calisio <carolina@unlockedlabs.org>
2025-05-27 15:27:50 -0400

    fix: add helper func to format timezone string

--- frontend/src/Components/helperFunctions/formatting.tsx
+++ frontend/src/Components/helperFunctions/formatting.tsx
@@ -1,3 +1,5 @@
+import { Timezones } from '@/common';
+
 export function formatPercent(value?: number | string): string {
     if (typeof value === 'string') return value;
     if (typeof value === 'number') return `${Math.round(value * 100) / 100}`;
@@ -15,4 +17,12 @@ export function transformStringToArray(input: string): string | string[] {
 export function parseLocalDay(isoDate: string): Date {
     const [year, month, day] = isoDate.split('-').map(Number);
     return new Date(year, month - 1, day);
+}
+
+export function toTimezoneString(timezoneValue: string) {
+    return (
+        Object.keys(Timezones).find(
+            (key) => Timezones[key as keyof typeof Timezones] === timezoneValue // eslint-disable-line
+        ) ?? timezoneValue
+    );
 }
