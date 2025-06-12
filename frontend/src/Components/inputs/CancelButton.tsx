960b56ef05805292f15adf66e8d1d03b85957664
carddev81 <rich@unlockedlabs.org>
2025-06-12 13:42:07 -0500

    fix: disable button on calendar when selected event is not the current class

--- frontend/src/Components/inputs/CancelButton.tsx
+++ frontend/src/Components/inputs/CancelButton.tsx
@@ -1,12 +1,15 @@
 export function CancelButton({
     label = 'Cancel',
+    disabled = false,
     onClick
 }: {
     label?: string;
+    disabled?: boolean;
     onClick: () => void;
 }) {
     return (
         <input
+            disabled={disabled}
             type="button"
             className="button-grey"
             onClick={onClick}
