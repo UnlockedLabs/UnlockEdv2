55ec3b17b61a04ea538c10338083ad76389d3cb5
calisio <carolina@unlockedlabs.org>
2025-06-16 12:30:30 -0400

    fix: conditionally set api endpoint for attendance modal, add error logic for userId

--- backend/src/handlers/class_events.go
+++ backend/src/handlers/class_events.go
@@ -142,10 +142,9 @@ func (srv *Server) handleGetProgramClassEvents(w http.ResponseWriter, r *http.Re
 	if err != nil {
 		return newInvalidIdServiceError(err, "class_id")
 	}
-	var userId *int
-	uid, err := strconv.Atoi(r.URL.Query().Get("user_id"))
-	if err == nil {
-		userId = &uid
+	userId, err := strconv.Atoi(r.URL.Query().Get("user_id"))
+	if err != nil {
+		log.errorf("Error parsing user id: %v", err)
 	}
 	log.add("class id", classID)
 	month := r.URL.Query().Get("month")
@@ -161,7 +160,7 @@ func (srv *Server) handleGetProgramClassEvents(w http.ResponseWriter, r *http.Re
 	}
 
 	qryCtx := srv.getQueryContext(r)
-	instances, err := srv.Db.GetClassEventInstancesWithAttendanceForRecurrence(classID, &qryCtx, month, year, userId)
+	instances, err := srv.Db.GetClassEventInstancesWithAttendanceForRecurrence(classID, &qryCtx, month, year, &userId)
 	if err != nil {
 		return newDatabaseServiceError(err)
 	}
