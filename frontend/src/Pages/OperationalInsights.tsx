ca6799d4d0ca3cf4677d8dc3e644ff4f056f5bb5
Richard Salas <rich@unlockedlabs.org>
2025-06-20 09:07:22 -0500

    fix: remove loader call and replace logic with facilities per ticket 310 (#923)

--- frontend/src/Pages/OperationalInsights.tsx
+++ frontend/src/Pages/OperationalInsights.tsx
@@ -1,14 +1,10 @@
-import { Facility } from '@/common';
 import OperationalInsights from '@/Components/OperationalInsightsCharts';
-import { useLoaderData } from 'react-router-dom';
 
 export default function OperationalInsightsPage() {
-    const facilities = useLoaderData() as Facility[];
-
     return (
         <div>
             <div className="w-full flex flex-col gap-6 px-5 pb-4">
-                <OperationalInsights facilities={facilities} />
+                <OperationalInsights />
             </div>
         </div>
     );
