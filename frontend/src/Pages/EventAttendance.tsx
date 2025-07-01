abb2e4066c63791e8cae64a5157d909ade3df46e
JamesUnlocked <torresjmo67@gmail.com>
2025-07-01 15:16:58 -0400

    feat: updated text on event button from 'back' to 'cancel' (#939)
    Co-authored-by: JamesUnlocked <james@unlockedlabs.org>

--- frontend/src/Pages/EventAttendance.tsx
+++ frontend/src/Pages/EventAttendance.tsx
@@ -462,7 +462,7 @@ export default function EventAttendance() {
                                 )
                             }
                         >
-                            Back
+                            Cancel
                         </button>
                         <button
                             type="submit"
