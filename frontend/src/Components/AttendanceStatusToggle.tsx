987ae7c2cab1efc2003ccfc580343f47d1febae6
jtucholski <josh.tucholski@gmail.com>
2025-07-12 07:10:27 -0400

    feat: improved ux on attendance (toggle, confirm all present)

---
+++ frontend/src/Components/AttendanceStatusToggle.tsx
@@ -0,0 +1,58 @@
+import {
+    CheckIcon,
+    ShieldCheckIcon,
+    XMarkIcon
+} from '@heroicons/react/24/outline';
+import ULIComponent from './ULIComponent';
+import { Attendance } from '@/common';
+
+interface AttendanceStatusToggleProps {
+    value?: Attendance;
+    onChange: (value: Attendance) => void;
+}
+
+export default function AttendanceStatusToggle({
+    value,
+    onChange
+}: AttendanceStatusToggleProps) {
+    return (
+        <div className="inline-flex w-fit gap-3">
+            <button
+                className={`flex flex-1 items-center justify-center gap-2 rounded-md bg-grey-1 px-4 py-2 text-base ${
+                    value === Attendance.Present
+                        ? 'bg-teal-3 opacity-70 text-white'
+                        : 'bg-grey-1 hover:bg-grey-2'
+                }`}
+                onClick={() => onChange(Attendance.Present)}
+                type="button"
+            >
+                <ULIComponent icon={CheckIcon} />
+                <label className="body-small cursor-pointer">Present</label>
+            </button>
+            <button
+                className={`flex flex-1 items-center justify-center gap-2 rounded-md px-4 py-2 text-base ${
+                    value === Attendance.Absent_Excused
+                        ? 'bg-pale-yellow text-base'
+                        : 'bg-grey-1 hover:bg-grey-2'
+                }`}
+                onClick={() => onChange(Attendance.Absent_Excused)}
+                type="button"
+            >
+                <ULIComponent icon={ShieldCheckIcon} />
+                <label className="body-small cursor-pointer">Excused</label>
+            </button>
+            <button
+                className={`flex flex-1 items-center justify-center gap-2 rounded-md bg-grey-1 px-4 py-2 text-base ${
+                    value === Attendance.Absent_Unexcused
+                        ? 'bg-red-2 opacity-80 text-white'
+                        : 'bg-grey-1 hover:bg-grey-2'
+                }`}
+                onClick={() => onChange(Attendance.Absent_Unexcused)}
+                type="button"
+            >
+                <ULIComponent icon={XMarkIcon} />
+                <label className="body-small cursor-pointer">Unexcused</label>
+            </button>
+        </div>
+    );
+}
--- frontend/src/Pages/EventAttendance.tsx
+++ frontend/src/Pages/EventAttendance.tsx
@@ -1,4 +1,4 @@
-import { startTransition, useEffect, useState } from 'react';
+import { startTransition, useEffect, useState, useRef } from 'react';
 import {
     useParams,
     useNavigate,
@@ -27,6 +27,13 @@ import { parseLocalDay } from '@/Components/helperFunctions/formatting';
 import { useToast } from '@/Context/ToastCtx';
 import { isCompletedCancelledOrArchived } from './ProgramOverviewDashboard';
 import { CancelButton } from '@/Components/inputs';
+import AttendanceStatusToggle from '@/Components/AttendanceStatusToggle';
+import {
+    TextOnlyModal,
+    TextModalType,
+    showModal,
+    closeModal
+} from '@/Components/modals';
 
 interface LocalRowData {
     selected: boolean;
@@ -81,6 +88,7 @@ export default function EventAttendance() {
         Record<number, LocalRowData>
     >({});
     const { toaster } = useToast();
+    const markAllPresentModal = useRef<HTMLDialogElement>(null);
     const {
         data: dates,
         error: datesError,
@@ -173,39 +181,6 @@ export default function EventAttendance() {
         }));
     }
 
-    function handleToggleSelect(user_id: number) {
-        const currentRow =
-            modifiedRows[user_id] || rows.find((r) => r.user_id === user_id);
-        const newSelected = !(currentRow?.selected ?? false);
-
-        setModifiedRows((prev) => ({
-            ...prev,
-            [user_id]: {
-                ...currentRow,
-                selected: newSelected,
-                attendance_status: newSelected
-                    ? currentRow?.attendance_status ?? Attendance.Present
-                    : undefined
-            }
-        }));
-    }
-    function handleSelectAll() {
-        const allSelected = rows.every((row) => row.selected);
-        setModifiedRows((prev) => {
-            const newModifications = { ...prev };
-            rows.forEach((row) => {
-                newModifications[row.user_id] = {
-                    ...(prev[row.user_id] || row),
-                    selected: !allSelected,
-                    attendance_status: !allSelected
-                        ? Attendance.Present
-                        : undefined
-                };
-            });
-            return newModifications;
-        });
-    }
-
     function handleAttendanceChange(user_id: number, newStatus: Attendance) {
         const currentRow = rows.find((r) => r.user_id === user_id) ?? {
             selected: false,
@@ -260,7 +235,9 @@ export default function EventAttendance() {
 
             return newMods;
         });
+        closeModal(markAllPresentModal);
     }
+
     async function onSubmit() {
         if (blockEdits) {
             toaster(
@@ -289,9 +266,9 @@ export default function EventAttendance() {
                     />
                 </div>
                 <button
-                    onClick={() => void handleMarkAllPresent()}
-                    disabled={anyRowSelected || blockEdits}
-                    className={`button ${tooltip} ${anyRowSelected ? `bg-grey-4 cursor-not-allowed` : ``}`}
+                    onClick={() => showModal(markAllPresentModal)}
+                    disabled={blockEdits}
+                    className={`button ${tooltip}`}
                     data-tip={
                         blockEdits
                             ? `This class is ${clsInfo?.status.toLowerCase()} and cannot be modified.`
@@ -324,9 +301,9 @@ export default function EventAttendance() {
                     >
                         <table className="table-2 mb-5">
                             <thead>
-                                <tr className="grid-cols-4 grid ">
+                                <tr className="grid-cols-[1fr_1fr_350px_1fr] grid gap-2">
                                     <th className="justify-self-start space-x-2 px-3">
-                                        <input
+                                        {/* <input
                                             type="checkbox"
                                             checked={
                                                 rows.length > 0 &&
@@ -334,11 +311,13 @@ export default function EventAttendance() {
                                             }
                                             onChange={handleSelectAll}
                                             className="checkbox cursor-pointer"
-                                        />
-                                        <span>Last, First</span>
+                                        /> */}
+                                        Name
                                     </th>
-                                    <th>Resident ID</th>
-                                    <th className="">Attendance</th>
+                                    <th className="justify-self-start">
+                                        Resident ID
+                                    </th>
+                                    <th className="">Status</th>
                                     <th className="">Note</th>
                                 </tr>
                             </thead>
@@ -346,10 +325,10 @@ export default function EventAttendance() {
                                 {rows.map((row) => (
                                     <tr
                                         key={row.user_id}
-                                        className="card w-full justify-items-center grid-cols-4 cursor-pointer p-2"
+                                        className="card w-full justify-items-center grid-cols-[1fr_1fr_350px_1fr] grid cursor-pointer p-2 gap-2"
                                     >
                                         <td className="justify-self-start space-x-2">
-                                            <input
+                                            {/* <input
                                                 type="checkbox"
                                                 checked={row.selected}
                                                 onChange={() =>
@@ -358,78 +337,30 @@ export default function EventAttendance() {
                                                     )
                                                 }
                                                 className="checkbox cursor-pointer"
-                                            />
-                                            <span>
-                                                {row.name_last},{' '}
-                                                {row.name_first}{' '}
-                                            </span>
+                                            /> */}
+                                            {row.name_last}, {row.name_first}{' '}
                                         </td>
-                                        <td className="items-center">
+                                        <td className="justify-self-start">
                                             {' '}
                                             {row.doc_id == ''
                                                 ? ' '
                                                 : `${row.doc_id}`}
                                         </td>
-                                        <td className="flex justify-center space-x-2">
-                                            <label className="flex items-center space-x-1 cursor-pointer">
-                                                <input
-                                                    type="radio"
-                                                    name={`attendance_${row.user_id}`}
-                                                    disabled={!row.selected}
-                                                    checked={
-                                                        row.attendance_status ===
-                                                            Attendance.Present ||
-                                                        (!row.attendance_status && // default to present when selected row
-                                                            row.selected)
-                                                    }
-                                                    onChange={() =>
-                                                        handleAttendanceChange(
-                                                            row.user_id,
-                                                            Attendance.Present
-                                                        )
-                                                    }
-                                                    className="radio cursor-pointer"
-                                                />
-                                                <span>Present</span>
-                                            </label>
-                                            <label className="flex items-center space-x-1 cursor-pointer">
-                                                <input
-                                                    type="radio"
-                                                    name={`attendance_${row.user_id}`}
-                                                    disabled={!row.selected}
-                                                    checked={
-                                                        row.attendance_status ===
-                                                        Attendance.Absent_Excused
-                                                    }
-                                                    onChange={() =>
-                                                        handleAttendanceChange(
-                                                            row.user_id,
-                                                            Attendance.Absent_Excused
-                                                        )
-                                                    }
-                                                    className="radio cursor-pointer"
-                                                />
-                                                <span>Absent - excused</span>
-                                            </label>
-                                            <label className="flex items-center space-x-1 cursor-pointer">
-                                                <input
-                                                    type="radio"
-                                                    name={`attendance_${row.user_id}`}
-                                                    disabled={!row.selected}
-                                                    checked={
-                                                        row.attendance_status ===
-                                                        Attendance.Absent_Unexcused
-                                                    }
-                                                    onChange={() =>
-                                                        handleAttendanceChange(
-                                                            row.user_id,
-                                                            Attendance.Absent_Unexcused
-                                                        )
-                                                    }
-                                                    className="radio cursor-pointer"
-                                                />
-                                                <span>Absent - unexcused</span>
-                                            </label>
+                                        <td className="flex justify-center">
+                                            <AttendanceStatusToggle
+                                                value={
+                                                    row.attendance_status ??
+                                                    (row.selected
+                                                        ? Attendance.Present
+                                                        : undefined)
+                                                }
+                                                onChange={(newStatus) =>
+                                                    handleAttendanceChange(
+                                                        row.user_id,
+                                                        newStatus
+                                                    )
+                                                }
+                                            />
                                         </td>
 
                                         <td className="justify-self-end align-middle">
@@ -483,6 +414,14 @@ export default function EventAttendance() {
                     />
                 </div>
             )}
+            <TextOnlyModal
+                ref={markAllPresentModal}
+                type={TextModalType.Confirm}
+                title="Mark All Present"
+                text={`Are you sure you want to mark all ${rows.length} residents on this page as Present? This action will override any existing attendance status.`}
+                onSubmit={() => void handleMarkAllPresent()}
+                onClose={() => void closeModal(markAllPresentModal)}
+            />
         </div>
     );
 }
