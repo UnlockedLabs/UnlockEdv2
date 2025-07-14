72878a5c89f8fe7666257d4e33cfb0e63e20fe95
jtucholski <josh.tucholski@gmail.com>
2025-07-14 09:36:24 -0400

    fix: added segregated to program drops

--- frontend/src/Pages/ClassEnrollmentDetails.tsx
+++ frontend/src/Pages/ClassEnrollmentDetails.tsx
@@ -190,7 +190,7 @@ export default function ClassEnrollmentDetails() {
         Completed = 'Completed',
         Withdrawn = 'Incomplete: Withdrawn',
         Dropped = 'Incomplete: Dropped',
-        Segregated = 'Incomplete: Segregated',
+        'Segregated - Dropped' = 'Incomplete: Segregated',
         'Failed To Complete' = 'Incomplete: Failed to Complete'
     }
     return (
