12e3773f1abce408638b34c9d760fc34e875e9c1
CK-7vn <keighan.robichaudgriffin@maine.edu>
2025-08-06 11:49:01 -0400

    fix: attendance renders when complete

--- frontend/src/Components/AttendanceStatusToggle.tsx
+++ frontend/src/Components/AttendanceStatusToggle.tsx
@@ -9,11 +9,13 @@ import { Attendance } from '@/common';
 interface AttendanceStatusToggleProps {
     value?: Attendance;
     onChange: (value: Attendance) => void;
+    disabled?: boolean;
 }
 
 export default function AttendanceStatusToggle({
     value,
-    onChange
+    onChange,
+    disabled = false
 }: AttendanceStatusToggleProps) {
     const buttonClass = `flex flex-1 items-center justify-center gap-2 rounded-md px-4 py-2 text-base`;
 
@@ -26,10 +28,12 @@ export default function AttendanceStatusToggle({
                         value === Attendance.Present
                             ? 'bg-teal-3 opacity-70 hover:shadow-md text-white'
                             : 'bg-grey-1 hover:bg-grey-2'
-                    }`
+                    }` +
+                    (disabled ? 'cursor-not-allowed opacity-50' : '')
                 }
                 onClick={() => onChange(Attendance.Present)}
                 type="button"
+                disabled={disabled}
             >
                 <ULIComponent icon={CheckIcon} />
                 <label className="body-small cursor-pointer">Present</label>
@@ -45,6 +49,7 @@ export default function AttendanceStatusToggle({
                 }
                 onClick={() => onChange(Attendance.Absent_Excused)}
                 type="button"
+                disabled={disabled}
             >
                 <ULIComponent icon={ShieldCheckIcon} />
                 <label className="body-small cursor-pointer">Excused</label>
@@ -60,6 +65,7 @@ export default function AttendanceStatusToggle({
                 }
                 onClick={() => onChange(Attendance.Absent_Unexcused)}
                 type="button"
+                disabled={disabled}
             >
                 <ULIComponent icon={XMarkIcon} />
                 <label className="body-small cursor-pointer">Unexcused</label>
