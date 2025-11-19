81d3b19c8f6de03ccc041ba8a574ac0b045de68b
Clyde Kallahan <keighan.robichaudgriffin@maine.edu>
2025-11-19 15:12:06 -0500

    fix: move conflict model

--- backend/src/models/program_classes.go
+++ backend/src/models/program_classes.go
@@ -271,3 +271,12 @@ func (pc *ProgramClass) GetProgramOwnerOrEmpty() string {
 	}
 	return ""
 }
+
+type ConflictDetail struct {
+	UserID           uint      `json:"user_id"`
+	UserName         string    `json:"user_name"`
+	ConflictingClass string    `json:"conflicting_class"`
+	ConflictStart    time.Time `json:"conflict_start"`
+	ConflictEnd      time.Time `json:"conflict_end"`
+	Reason           string    `json:"reason"`
+}
