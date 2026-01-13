052989a12b7de35556211d9b7dd000fa195aa7ff
carddev81 <rich@unlockedlabs.org>
2026-01-13 13:06:34 -0600

    fix: add conditional check for whether user is in session to determine navigation

--- config/zims.sh
+++ config/zims.sh
@@ -6,9 +6,9 @@ ZIM_COUNT=$(find ./csvs/zims -type f -name "*.zim" | wc -l)
 
 if [ "$ZIM_COUNT" -eq 0 ]; then
     echo "No ZIM files found. Downloading..."
-    wget -q https://download.kiwix.org/zim/devdocs/devdocs_en_go_2025-07.zim -P ./csvs/zims
-    wget -q https://download.kiwix.org/zim/devdocs/devdocs_en_bash_2025-07.zim -P ./csvs/zims
-    wget -q https://download.kiwix.org/zim/devdocs/devdocs_en_c_2025-07.zim -P ./csvs/zims
+    wget -q https://download.kiwix.org/zim/devdocs/devdocs_en_go_2026-01.zim -P ./csvs/zims
+    wget -q https://download.kiwix.org/zim/devdocs/devdocs_en_bash_2026-01.zim -P ./csvs/zims
+    wget -q https://download.kiwix.org/zim/devdocs/devdocs_en_c_2026-01.zim -P ./csvs/zims
 else
     echo "ZIM files already exist. No download needed."
 fi
