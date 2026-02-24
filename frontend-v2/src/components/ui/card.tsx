07ce5925f8cd7719b3d3ddaf2a3119ce91a8ffcf
Cory Pride <cory.pride83@gmail.com>
2026-02-23 19:04:28 -0600

    fix: adjusted missing styling

--- frontend-v2/src/components/ui/card.tsx
+++ frontend-v2/src/components/ui/card.tsx
@@ -65,7 +65,7 @@ function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-content"
-      className={cn("px-6 [&:last-child]:", className)}
+      className={cn("px-6 pb-6", className)}
       {...props}
     />
   );
